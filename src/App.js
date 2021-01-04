0219b7d3f2ff28a853b2be8fc75d9ecc3d9bc08f
Alex Gordon <alexgordon@Alexs-Mac-mini.018.co.il>
2021-01-04 12:22:37 +0200

    Added chart component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import Details from './components/Details'
+import {Grid} from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
-    <center>dada</center>hidad
+    <Grid container spacine={0} aliginItems="center" justify="center" style={{height: '100vh'}}>
+      <Grid item xs={12} sm ={4}>
+      <Details/>
+      </Grid>
+      <Grid item xs={12} sm ={4}>
+        Main
+      </Grid>
+      <Grid item xs={12} sm ={4}>
+      <Details/>
+      </Grid>
+    </Grid>
+    
     </div>
   );
 }
