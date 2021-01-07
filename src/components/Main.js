35f1eca1238ee96ef1ef608212ac1cca595a3c69
alexgordontlv <alexgordon@Alexs-Mac-mini.018.co.il>
2021-01-07 19:51:08 +0200

    test

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,7 +9,7 @@ const Main = () => {
         <Card className={classes.root}>
             <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
             <CardContent>
-                <Typography align="center" variant="h5">Total Balance $100</Typography>
+                <Typography align="center" variant="h5">Total Balance $1000</Typography>
                 <Typography variant="subtitle1" style={{lineHeight: '1.5em', marginTop:'20px'}}>
                 </Typography>
                 <Divider/>
