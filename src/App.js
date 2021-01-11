9b31dab28a140a4533f4a99fd1d4367863788c87
alexgordontlv <alexaybn@gmail.com>
2021-01-11 19:02:25 +0200

    changed grid styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import './App.css';
-import Details from './components/Details'
-import {Grid} from '@material-ui/core'
-import useStyles from './styles'
-import Main from './components/Main';
-
+import React from "react";
+import "./App.css";
+import Details from "./components/Details";
+import { Grid } from "@material-ui/core";
+import useStyles from "./styles";
+import Main from "./components/Main";
 
 function App() {
   const classes = useStyles();
 
   return (
     <div>
-    <Grid  className={classes.grid} container spacine={0} alignItems="center" justify="center" style={{height: '100vh'}}>
-      <Grid item xs={12} sm ={4}>
-      <Details title="Income"/>
+      <Grid
+        className={classes.grid}
+        container
+        spacine={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Main />
+        </Grid>
+        <Grid item xs={12} sm={3} spacing={2}>
+          <Details title="Income" />
+          <Details title="Expense" />
+        </Grid>
       </Grid>
-      <Grid item xs={12} sm ={3}>
-        <Main/>
-      </Grid>
-      <Grid item xs={12} sm ={4}>
-      <Details title="Expense"/>
-      </Grid>
-    </Grid>
-    
     </div>
   );
 }
